f30b0c9e630864cc240ead49c04bdbbadd99d6ec
Andrew Bolt <andrewebolt@gmail.com>
2014-06-05 15:21:41 -0400

    Exercise 1.7 complete

--- people.rb
+++ people.rb
@@ -1,11 +1,28 @@
-class Student
-	def learn()
+class People
+	attr_accessor :name
+
+	def greeting
+		puts "Hi, my name is #{@name}"
+	end
+end
+
+class Student < People
+	def learn
 		puts "I get it!"
 	end
 end
 
-class Instructor
+class Instructor < People
 	def teach
 		puts "Everything in Ruby is an Object"
 	end
 end
+
+instructor_1 = Instructor.new
+instructor_1.name = "Chris"
+instructor_1.greeting
+
+
+student_1 = Student.new
+student_1.name = "Christina"
+student_1.greeting 
