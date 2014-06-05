9a8974a9c0879a4793f7abda2aa91f156e95b833
Andrew Bolt <andrewebolt@gmail.com>
2014-06-05 15:26:38 -0400

    Exercise 1.8 complete

--- people.rb
+++ people.rb
@@ -25,4 +25,12 @@ instructor_1.greeting
 
 student_1 = Student.new
 student_1.name = "Christina"
-student_1.greeting 
+student_1.greeting
+
+instructor_1.teach
+student_1.learn 
+
+#Calling the teach method on student_1 doesn't work because it is not 
+#part of the Instructor class. The teach method is part of the 
+#Instructor class and therefore can not be called on the Student class.
+student_1.teach
